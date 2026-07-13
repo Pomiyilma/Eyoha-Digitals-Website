0c1a6640c8a27414b14c99e815a3b3da9f0294e2
Pomiyilma <pomiyilma@gmail.com>
2026-07-13 17:43:49 +0300

    Fixed import issues

--- landing_page/src/app/Details/[id]/page.tsx
+++ landing_page/src/app/Details/[id]/page.tsx
@@ -10,6 +10,7 @@ import { Testimonial } from "@/sectionsclientdetailpage/Testimonial";
 import Navbar2 from "@/sections/Navbar2";
 import Footer from "@/sections/Footer";
 import Image from "next/image";
+import { CheckCircle2, ArrowRight} from "lucide-react";
 
 const CaseStudyTemplate = () => {
     const params = useParams();
